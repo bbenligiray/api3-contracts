4737c6182bc452e19a6bbb5f034acdde6732ccbc
Burak Benligiray <bbenligiray@gmail.com>
2024-01-18 13:39:51 +0300

    Start adding AirseekerRegistry docstrings

--- contracts/AirseekerRegistry.sol
+++ contracts/AirseekerRegistry.sol
@@ -7,6 +7,19 @@ import "./interfaces/IAirseekerRegistry.sol";
 import "./vendor/@openzeppelin/contracts@4.9.5/utils/structs/EnumerableSet.sol";
 import "@api3/airnode-protocol-v1/contracts/api3-server-v1/interfaces/IApi3ServerV1.sol";
 
+/// @title A contract that is used by its owner to register the data required
+/// by an Airseeker to uphold the respective data feed specs
+/// @notice Airseeker is an application that pushes API provider-signed data to
+/// chain when certain conditions are met so that the data feeds served on the
+/// Api3ServerV1 contract are updated according to the respective specs. In
+/// other words, this contract can be thought of as an on-chain configuration
+/// file for an Airseeker (or multiple Airseekers with redundancy).
+/// The Airseeker must know which data feeds are active (and thus need to be
+/// updated), the constituting Airnode (the oracle node that API providers
+/// operate to sign data) addresses and request template IDs, what the
+/// respective on-chain data feed values are, what the update parameters are,
+/// and the URL of the signed APIs (from which Airseeker can fetch signed data)
+/// that are hosted by the respective API providers.
 contract AirseekerRegistry is
     Ownable,
     ExtendedSelfMulticall,
@@ -14,49 +27,87 @@ contract AirseekerRegistry is
 {
     using EnumerableSet for EnumerableSet.Bytes32Set;
 
+    /// @notice Maximum number of Beacons that a Beacon set whose details are
+    /// being registered can consist of
+    /// @dev A Beacon is identified by an Airnode address and a request
+    /// template ID, which means it is a single-source data feed. Multiple
+    /// Beacons can be aggregated on-chain to create a multiple-source data
+    /// feed, which is called a Beacon set. This conract does not support
+    /// Beacon sets that consist of more than `MAXIMUM_BEACON_COUNT_IN_SET`
+    /// Beacons. In the context of a first-party oracle solution, this is the
+    /// maximum number of API providers.
     uint256 public constant override MAXIMUM_BEACON_COUNT_IN_SET = 21;
 
+    /// @notice Api3ServerV1 contract address
     address public immutable override api3ServerV1;
 
+    /// @notice Airnode address to signed API URL
+    /// @dev The signed APIs must implement the specific interface that
+    /// Airseeker expects. Refer to https://github.com/api3dao/signed-api for
+    /// an example implementation.
+    /// The signed APIs must be hosted by the operator of the respective
+    /// Airnode (i.e., the API provider) for a trust-minimized architecture.
     mapping(address => string) public override airnodeToSignedApiUrl;
 
+    /// @notice Data feed ID to encoded details
+    /// @dev See the `registerDataFeed()` implementation for details
     mapping(bytes32 => bytes) public override dataFeedIdToDetails;
 
+    // Api3ServerV1 uses Beacon IDs (see the `deriveBeaconId()` implementation)
+    // and Beacon set IDs (see the `deriveBeaconSetId()` implementation) to
+    // address data feeds. We use data feed ID as a general term to refer to a
+    // Beacon or Beacon set ID.
+    // A data feed ID is immutable. Api3ServerV1 allows a dAPI name to be
+    // pointed to a data feed ID to implement a mutable data feed addressing
+    // scheme.
+    // If the data feed ID or dAPI name should be used to read a data feed
+    // depends on the use case. To support both schemes, AirseekerRegistry
+    // allows data feeds specs to be defined with either the data feed ID or
+    // the dAPI name.
+    EnumerableSet.Bytes32Set private activeDataFeedIds;
+
+    EnumerableSet.Bytes32Set private activeDapiNames;
+
+    // Considering that the update parameters are typically reused, a hash map
+    // is used to avoid storing the same update parameters redundantly
     mapping(bytes32 => bytes32) private dataFeedIdToUpdateParametersHash;
 
     mapping(bytes32 => bytes32) private dapiNameToUpdateParametersHash;
 
     mapping(bytes32 => bytes) private updateParametersHashToValue;
 
-    EnumerableSet.Bytes32Set private activeDataFeedIds;
-
-    EnumerableSet.Bytes32Set private activeDapiNames;
-
-    // The length of abi.encode(address,bytes32)
+    // Length of abi.encode(address, bytes32)
     uint256 private constant DATA_FEED_DETAILS_LENGTH_FOR_SINGLE_BEACON =
         32 + 32;
 
+    // Length of abi.encode(address[2], bytes32[2])
     uint256
         private constant DATA_FEED_DETAILS_LENGTH_FOR_BEACON_SET_WITH_TWO_BEACONS =
         (2 * 32) + (32 + 2 * 32) + (32 + 2 * 32);
 
-    // The length of abi.encode(address[],bytes32[]), where each array has
-    // MAXIMUM_BEACON_COUNT_IN_SET items
+    // Length of
+    // abi.encode(address[MAXIMUM_BEACON_COUNT_IN_SET], bytes32[MAXIMUM_BEACON_COUNT_IN_SET])
     uint256 private constant MAXIMUM_DATA_FEED_DETAILS_LENGTH =
         (2 * 32) +
             (32 + MAXIMUM_BEACON_COUNT_IN_SET * 32) +
             (32 + MAXIMUM_BEACON_COUNT_IN_SET * 32);
 
+    /// @dev Reverts if the data feed ID is zero
+    /// @param dataFeedId Data feed ID
     modifier onlyNonZeroDataFeedId(bytes32 dataFeedId) {
         require(dataFeedId != bytes32(0), "Data feed ID zero");
         _;
     }
 
+    /// @dev Reverts if the dAPI name is zero
+    /// @param dapiName dAPI name
     modifier onlyNonZeroDapiName(bytes32 dapiName) {
         require(dapiName != bytes32(0), "dAPI name zero");
         _;
     }
 
+    /// @param owner_ Owner address
+    /// @param api3ServerV1_ Api3ServerV1 contract address
     constructor(address owner_, address api3ServerV1_) {
         require(owner_ != address(0), "Owner address zero");
         require(api3ServerV1_ != address(0), "Api3ServerV1 address zero");
@@ -64,16 +115,18 @@ contract AirseekerRegistry is
         api3ServerV1 = api3ServerV1_;
     }
 
-    function owner() public view override(IOwnable, Ownable) returns (address) {
+    /// @notice Returns the owner address
+    /// @return Owner address
+    function owner() public view override(Ownable, IOwnable) returns (address) {
         return super.owner();
     }
 
-    // Disabled ownership renouncing and transfers to enable this to be
-    // deployed deterministically
+    /// @notice Overriden to be disabled
     function renounceOwnership() public pure override(IOwnable, Ownable) {
         revert("Ownership cannot be renounced");
     }
 
+    /// @notice Overriden to be disabled
     function transferOwnership(
         address
     ) public pure override(IOwnable, Ownable) {
@@ -116,6 +169,7 @@ contract AirseekerRegistry is
         bytes32 dataFeedId,
         bytes calldata updateParameters
     ) external override onlyOwner onlyNonZeroDataFeedId(dataFeedId) {
+        // Set update parameters maximum length
         bytes32 updateParametersHash = keccak256(updateParameters);
         if (
             dataFeedIdToUpdateParametersHash[dataFeedId] != updateParametersHash
